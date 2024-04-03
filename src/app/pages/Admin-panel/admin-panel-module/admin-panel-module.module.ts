a6b0479f761a74d66178f57b124907c608b5998f
Hasnain Aslam <hasnainaslam808@gmail.com>
2024-04-03 23:12:22 +0500

    dashboard

--- src/app/pages/Admin-panel/admin-panel-module/admin-panel-module.module.ts
+++ src/app/pages/Admin-panel/admin-panel-module/admin-panel-module.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    CategoriesComponent,
     
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
-      { path: '', component:DashboardComponent }
+      { path: '', component:CategoriesComponent }
 
     ])
   ]
