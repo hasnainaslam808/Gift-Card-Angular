64f67b425ce0bc1d5e4c626aa226a82f76ff39c4
Hasnain Aslam <hasnainaslam808@gmail.com>
2024-04-17 10:40:13 +0500

    updation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/Admin-panel/admin-panel-module/profile/profile.component';
+import { SettingComponent } from './pages/Admin-panel/admin-panel-module/setting/setting.component';
 const routes: Routes = [
 
   {path: '', component:LoginComponent,data:{currentRoute:'login',header:false }},
+  {path: 'login', component:LoginComponent,data:{currentRoute:'login',header:false }},
   {path:'reset-password', component:LoginComponent,data:{currentRoute:'reset-password' ,header:false }},
   {path:'emial-verification', component:LoginComponent,data:{currentRoute:'emial-verification' ,header:false }},
   {path:'set-new-password', component:LoginComponent,data:{currentRoute:'set-new-password' ,header:false }},
@@ -11,8 +14,9 @@ const routes: Routes = [
   {
     path: "dashboard",
     loadChildren: () => import('./pages/Admin-panel/admin-panel-module/admin-panel-module.module').then((m) => m.AdminPanelModuleModule)
-  }
-  
+  },
+{path:'profile',component:ProfileComponent} , 
+{path:'setting',component:SettingComponent} 
 
 
 ];
