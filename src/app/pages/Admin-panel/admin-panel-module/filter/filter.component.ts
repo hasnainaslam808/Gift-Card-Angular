f9b779c70ee7d450d612d62b9a2318603388079a
Hasnain Aslam <hasnainaslam808@gmail.com>
2024-04-19 15:22:53 +0500

    updation till final phase

--- src/app/pages/Admin-panel/admin-panel-module/filter/filter.component.ts
+++ src/app/pages/Admin-panel/admin-panel-module/filter/filter.component.ts
@@ -11,5 +11,7 @@ export class FilterComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  currentActiveBtn(res: any) {
+    this.activeBtn = res
+  }   activeBtn: string = 'filter'
 }
--- src/app/pages/Admin-panel/admin-panel-module/user-detail/user-detail.component.ts
+++ src/app/pages/Admin-panel/admin-panel-module/user-detail/user-detail.component.ts
@@ -6,7 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.scss']
 })
 export class UserDetailComponent implements OnInit {
+  tableData=[
+    {id:1, cardID:233423, email:"wassi@gmail.com",users:"Wassi Ahsan",brand:"KFC",amount:23,giftfor:"Myself"},
+    {id:2, cardID:233423, email:"wassi@gmail.com",users:"Wassi Ahsan",brand:"KFC",amount:23,giftfor:"Myself"},
+    {id:3, cardID:233423, email:"wassi@gmail.com",users:"Wassi Ahsan",brand:"KFC",amount:23,giftfor:"Myself"},
+    {id:4, cardID:233423, email:"wassi@gmail.com",users:"Wassi Ahsan",brand:"KFC",amount:23,giftfor:"Myself"},
+    {id:5, cardID:233423, email:"wassi@gmail.com",users:"Wassi Ahsan",brand:"KFC",amount:23,giftfor:"Myself"},
+    {id:1, cardID:233423, email:"wassi@gmail.com",users:"Wassi Ahsan",brand:"KFC",amount:23,giftfor:"Myself"},
+    {id:2, cardID:233423, email:"wassi@gmail.com",users:"Wassi Ahsan",brand:"KFC",amount:23,giftfor:"Myself"},
+    {id:3, cardID:233423, email:"wassi@gmail.com",users:"Wassi Ahsan",brand:"KFC",amount:23,giftfor:"Myself"},
+    {id:4, cardID:233423, email:"wassi@gmail.com",users:"Wassi Ahsan",brand:"KFC",amount:23,giftfor:"Myself"},
+    {id:5, cardID:233423, email:"wassi@gmail.com",users:"Wassi Ahsan",brand:"KFC",amount:23,giftfor:"Myself"},
+    {id:1, cardID:233423, email:"wassi@gmail.com",users:"Wassi Ahsan",brand:"KFC",amount:23,giftfor:"Myself"},
+    {id:2, cardID:233423, email:"wassi@gmail.com",users:"Wassi Ahsan",brand:"KFC",amount:23,giftfor:"Myself"},
+    {id:3, cardID:233423, email:"wassi@gmail.com",users:"Wassi Ahsan",brand:"KFC",amount:23,giftfor:"Myself"},
+    {id:4, cardID:233423, email:"wassi@gmail.com",users:"Wassi Ahsan",brand:"KFC",amount:23,giftfor:"Myself"},
+    {id:5, cardID:233423, email:"wassi@gmail.com",users:"Wassi Ahsan",brand:"KFC",amount:23,giftfor:"Myself"},
 
+  ]
+  pageSize=5;
+  currentPage=1;
   constructor() { }
 
   ngOnInit(): void {
